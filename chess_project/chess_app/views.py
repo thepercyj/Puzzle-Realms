72685e86a788edc11434e29126b35082b98d94fd
Aman Thapa Magar (PJ) <aman.yhbs@gmail.com>
2023-10-17 17:41:04 +0100

    Nqueens Logic Installed
    Added Nqueens logic with dynamic chessboard view. Followed M-V-T Pattern for the architecture, deleted exisitng chess_project which as dummy product. Also, fixed some minor table views.
    
    Done on 10/17/2023

--- chess_project/chess_app/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-from django.shortcuts import render
-
-
-def chess_board(request):
-    return render(request, "chess_app/chess_board.html")
-
