1303cc1254bfc83c520dfd32502c8135e29cf22e
Aman Thapa Magar (PJ) <aman.yhbs@gmail.com>
2023-10-20 15:29:55 +0100

    Changes done to URLs.
    changes done to the url.
    
    updated on 10/20/2023

--- n_queens/nqueens_app/urls.py
+++ n_queens/nqueens_app/urls.py
@@ -4,6 +4,5 @@ from . import views
 # The routing of the view file is defined and passed as a path inside here.
 
 urlpatterns = [
-    path('nqueens/', views.nqueens, name='nqueens'),
-    path('', views.nqueens, name='nqueens_default'),
+    path('', views.nqueens, name='nqueens'),
 ]
