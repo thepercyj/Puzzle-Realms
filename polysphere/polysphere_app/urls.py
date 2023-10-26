a5438a501274952bdd23ee0a3e96dc712ef9e5ed
Tom Naccarato <108072398+tnaccarato@users.noreply.github.com>
2023-10-26 20:15:42 +0100

    Update url

--- polysphere/polysphere_app/urls.py
+++ polysphere/polysphere_app/urls.py
@@ -4,5 +4,5 @@ from . import views
 # The routing of the view file is defined and passed as a path inside here.
 
 urlpatterns = [
-    path('', views.polysphere, name='polysphere'),
+    path('', views.game_board, name='game_board'),
 ]
