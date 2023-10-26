beb126b6b887a38417f5f6d33e27c2156114b762
Tom Naccarato <108072398+tnaccarato@users.noreply.github.com>
2023-10-26 20:03:19 +0100

    Rename view

--- polysphere/polysphere_app/views.py
+++ polysphere/polysphere_app/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def game_board(request):
-    return render(request, 'game_board.html')
+    return render(request, 'polysphere.html')
