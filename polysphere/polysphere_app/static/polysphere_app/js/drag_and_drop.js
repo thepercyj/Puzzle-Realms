02f697e9c4ca7d4b13cedab10f31a22baac1371c
Tom Naccarato <108072398+tnaccarato@users.noreply.github.com>
2023-10-26 20:37:24 +0100

    Add logic to stop overlap

--- polysphere/polysphere_app/static/polysphere_app/js/drag_and_drop.js
+++ polysphere/polysphere_app/static/polysphere_app/js/drag_and_drop.js
@@ -3,8 +3,9 @@ $(document).ready(function() {
     $('.game-piece').draggable({
         containment: 'body', // Optional: Restrict dragging within the body
         cursor: 'move', // Optional: Change cursor on drag
-        snap: '.grid-cell', // Optional: Snap to grid cells (if you have a grid)
-        snapMode: 'inner' // Optional: Snap to the center of grid cells
+        snap: '.grid-cell', // Snap to grid cells
+        snapMode: 'inner', // Snap to the center of grid cells
+        snapTolerance: 20 // Adjust the tolerance to control snapping behavior
     });
 
     // Handle the drop event
@@ -14,12 +15,48 @@ $(document).ready(function() {
             // Get the dragged game piece element
             var droppedPiece = ui.draggable;
 
-            // Get the piece's data attributes
-            var pieceId = droppedPiece.data('piece'); // Get the piece ID
-            var orientation = droppedPiece.data('orientation'); // Get the orientation
+            // Find the nearest unoccupied grid cell
+            var nearestCell = findNearestUnoccupiedCell($(this));
+
+            // Snap the game piece to the nearest unoccupied cell
+            droppedPiece.position({
+                of: nearestCell,
+                my: 'center',
+                at: 'center'
+            });
 
             // Perform game logic based on pieceId and orientation
             // For example, update the game state, check for valid moves, etc.
         }
     });
+
+    // Function to find the nearest unoccupied grid cell
+    function findNearestUnoccupiedCell(targetCell) {
+        var nearestCell = null;
+        var minDistance = Number.MAX_VALUE;
+
+        $('.grid-cell').each(function() {
+            var cell = $(this);
+
+            // Check if the cell is unoccupied
+            if (!cell.hasClass('occupied')) {
+                // Calculate distance between the target cell and the current cell
+                var distance = Math.sqrt(
+                    Math.pow(targetCell.position().left - cell.position().left, 2) +
+                    Math.pow(targetCell.position().top - cell.position().top, 2)
+                );
+
+                // Update nearestCell if this cell is closer
+                if (distance < minDistance) {
+                    nearestCell = cell;
+                    minDistance = distance;
+                }
+            }
+        });
+
+        // Mark the nearest cell as occupied
+        nearestCell.addClass('occupied');
+
+        return nearestCell;
+    }
 });
