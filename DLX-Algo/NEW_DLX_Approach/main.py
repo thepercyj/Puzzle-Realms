53497967331af64fb094b1be83e3407548471bd7
Rez27 <kadam09rohan@gmail.com>
2023-11-08 00:35:31 +0000

    Partial configuration bug
    The partial configuration does not work and just places the first letter initialized in the InitializeBoard function. Complete configuration works

--- DLX-Algo/NEW_DLX_Approach/main.py
+++ DLX-Algo/NEW_DLX_Approach/main.py
@@ -10,8 +10,8 @@ import kanoodle
 from kanoodle import *
 
 # Define grid dimensions
-GridWidth = 3
-GridHeight = 4
+GridWidth = 4
+GridHeight = 3
 
 # Define the pieces
 Pieces = [
@@ -21,28 +21,33 @@ Pieces = [
     # "J   \n"+
     # "JJ  \n"+
     # "J   \n",
-
-    " L  \n"+
-    " L  \n"+
-    " L  \n"
+    #
+    " I \n"+
+    " I \n"+
+    " I \n"
 ]
 
 def main():
     start_time = time.time()
 
     # Initialize the board with 2 pieces
-    # initial_board = [[' ' for _ in range(GridWidth)] for _ in range(GridHeight)]
+    initial_board = [[' ' for _ in range(GridWidth)] for _ in range(GridHeight)]
+
     # Initialize the board with empty spaces
-    initial_board = [[' ' for j in range(GridWidth)] for i in range(GridHeight)]
+    #initial_board = [[' ' for j in range(GridWidth)] for i in range(GridHeight)]
     initialize_board(initial_board)   # Convert the board to a string format
+
     board_description = ""
     for i in range(GridHeight):
         for j in range(GridWidth):
             board_description += initial_board[i][j]
         board_description += "\n"
 
+
     # Add the board description to the list of pieces
     modified_pieces = Pieces + [board_description]
+
+
 
     # Use the modified_pieces array to find solutions
     # This assumes the existence of a Kanoodle class with a findAllSolutions method, which is not provided.
@@ -50,23 +55,22 @@ def main():
     end_time = time.time()
 
     if answer:
-        print("Found answer:\n", answer)
+        print("Found answer:\n", answer, "\n in", )
 
 def initialize_board(board):
     # Example: Place piece I at (0, 0) and piece I at (0, 2)
     # Initialize the board with empty spaces already handled in the main function
-    print("This is board", board)
 
-    board[0][0] = 'I'
-    board[0][1] = 'I'
-    board[1][0] = 'I'
-    board[2][0] = 'I'
-    board[2][1] = 'I'
+    board[0][0] = 'J'
+    board[0][1] = 'J'
+    board[1][0] = 'J'
+    board[2][0] = 'J'
+    board[2][1] = 'J'
 
-    board[0][2] = 'J'
-    board[1][1] = 'J'
-    board[1][2] = 'J'
-    board[2][2] = 'J'
+    board[0][2] = 'L'
+    board[1][1] = 'L'
+    board[1][2] = 'L'
+    board[2][2] = 'L'
 
 if __name__ == "__main__":
     main()
