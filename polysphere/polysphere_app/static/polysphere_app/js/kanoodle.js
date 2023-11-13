5b94bb088e4de69e2d8e4d11ec34e877039b2bad
Tom Naccarato <tcarlnaccarato@gmail.com>
2023-11-13 04:41:13 +0000

    BUG: Horizontal flip is getting the mirror
    Added logic to update the coordinates of the pieces on the grid, but a logical error causing pieces to get the exact mirror if flipped and rotated 180 or 360 degrees
    
    Co-Authored-By: Aman Thapa Magar (PJ) <20582994+thepercyj@users.noreply.github.com>
    Co-Authored-By: Rez27 <46955742+Rez27@users.noreply.github.com>

--- polysphere/polysphere_app/static/polysphere_app/js/kanoodle.js
+++ polysphere/polysphere_app/static/polysphere_app/js/kanoodle.js
@@ -22,46 +22,51 @@ window.onload = function () {
     ];
     let gridColor = ["#2F922C", "#672598", "#0A8286", "#9C3A3A", "#A0A125", "#9B108E", "#9A5835", "#223A21", "#191A92", "#946D98", "#256191", "#A0A467"];
     let pieces = [
-        [[0,-1],[0,0],[0,1],
-         [1,-1],     [1,1]],
+        [[0, -1], [0, 0], [0, 1],
+            [1, -1], [1, 1]],
 
-        [              [0,0],[0,1],
-         [1,-2],[1,-1],[1,0]],
+        [[0, 0], [0, 1],
+            [1, -2], [1, -1], [1, 0]],
 
-        [       [-1,0],
-         [0,-1],[0,0],
-                [1,0],[1,1]],
+        [[-1, 0],
+            [0, -1], [0, 0],
+            [1, 0], [1, 1]],
 
-        [      [-1,0],
-         [0,-1],[0,0],[0,1]],
+        [[-1, 0],
+            [0, -1], [0, 0], [0, 1]],
 
-        [       [-1,0],
-         [0,-1],[0,0],[0,1],[0,2]],
+        [[-1, 0],
+            [0, -1], [0, 0], [0, 1], [0, 2]],
 
-        [       [0,0],[0,1],
-         [1,-1],[1,0],[1,1]],
+        [[0, 0], [0, 1],
+            [1, -1], [1, 0], [1, 1]],
 
-        [       [0,0],[0,1],
-         [1,-1],[1,0]],
+        [[0, 0], [0, 1],
+            [1, -1], [1, 0]],
 
-        [[0,0],[0,1],
-         [1,0],
-         [2,0]],
+        [[0, 0], [0, 1],
+            [1, 0],
+            [2, 0]],
 
-        [[0,-2],[0,-1],[0,0],
-                       [1,0],
-                       [2,0]],
+        [[0, -2], [0, -1], [0, 0],
+            [1, 0],
+            [2, 0]],
 
-        [[-1,0],
-         [0,0],[0,1],[0,2],[0,3]],
+        [[-1, 0],
+            [0, 0], [0, 1], [0, 2], [0, 3]],
 
-        [[-1,0],
-         [0,0],[0,1]],
+        [[-1, 0],
+            [0, 0], [0, 1]],
 
-        [[-1,-1],[-1,0],
-                 [0,0],[0,1],
-                       [1,1]],
+        [[-1, -1], [-1, 0],
+            [0, 0], [0, 1],
+            [1, 1]],
     ];
+
+    // Stores the orientation and flip status of the pieces
+    let pieceRotation = new Array(12).fill(0)
+    let horizontalFlip = new Array(12).fill(false)
+    let verticalFlip = new Array(12).fill(false)
 
     function createBoardMatrix() {
         const matrix = [];
@@ -93,21 +98,36 @@ window.onload = function () {
 
     function rotateClockwise() {
         rotationAngle = (rotationAngle + 90) % 360;
+        pieceRotation[currentIndex]  = rotationAngle
         updateImage();
     }
 
     function rotateCounterclockwise() {
         rotationAngle = (rotationAngle - 90 + 360) % 360;
+        pieceRotation[currentIndex] = rotationAngle;
         updateImage();
     }
 
     function flipHorizontal() {
-        currentImage.style.transform = `scaleX(-1) rotate(${rotationAngle}deg)`;
-    }
+    horizontalFlip[currentIndex] = !horizontalFlip[currentIndex];
+    applyCurrentTransformations(); // Apply current transformations to the image
+}
 
-    function flipVertical() {
-        currentImage.style.transform = `scaleY(-1) rotate(${rotationAngle}deg)`;
+function flipVertical() {
+    verticalFlip[currentIndex] = !verticalFlip[currentIndex];
+    applyCurrentTransformations(); // Apply current transformations to the image
+}
+
+function applyCurrentTransformations() {
+    let transform = `rotate(${rotationAngle}deg)`;
+    if (horizontalFlip[currentIndex]) {
+        transform += ' scaleX(-1)';
     }
+    if (verticalFlip[currentIndex]) {
+        transform += ' scaleY(-1)';
+    }
+    currentImage.style.transform = transform;
+}
 
     const previousImageButton = document.getElementById('previousImageButton');
     const nextImageButton = document.getElementById('nextImageButton');
@@ -156,6 +176,10 @@ window.onload = function () {
 
     function dragStart(event) {
         event.dataTransfer.setData('text/plain', event.target.id);
+        // Updates the rotation state of the piece being dragged
+        const pieceId = event.target.id.replace('piece-', 'shape-')
+        const pieceIndex = imageIds.indexOf((pieceId));
+        pieceRotation[pieceIndex] = rotationAngle;
     }
 
     function drop(event) {
@@ -167,11 +191,13 @@ window.onload = function () {
 
         if (cell.classList.contains('cell')) {
             const pieceCoords = pieces[currentIndex];
+            const rotatedPieceCoords = rotateCoords(pieceCoords, pieceRotation[currentIndex])
+            const flippedPieceCoords = applyFlips(rotatedPieceCoords, currentIndex)
 
             // Check if the cells for the new piece are unoccupied
-            if (isSpaceAvailable(row, col, pieceCoords)) {
+            if (isSpaceAvailable(row, col, flippedPieceCoords)) {
                 // Iterate through the coordinates of the current piece
-                for (const coord of pieceCoords) {
+                for (const coord of flippedPieceCoords) {
                     const newRow = row + coord[0];
                     const newCol = col + coord[1];
 
@@ -210,4 +236,39 @@ window.onload = function () {
         currentImage.style.left = '0';
         currentImage.style.top = '0';
     }
-};
+
+    function rotateCoords(coords, angle) {
+        return coords.map(coord => {
+                switch (angle) {
+                    case 90:
+                        return [-coord[1], coord[0]];
+                    case 180:
+                        return [-coord[0], -coord[1]];
+                    case 270:
+                        return [coord[1], -coord[0]];
+                    default:
+                        return coord;
+                }
+            }
+        );
+    }
+
+    function applyFlips(coords, index) {
+        let transformedCoords = coords;
+        if (horizontalFlip[index]) {
+            transformedCoords = transformedCoords.map(coord => [-coord[0], coord[1]]);
+        }
+        if (verticalFlip[index]) {
+            transformedCoords = transformedCoords.map(coord => [coord[0], -coord[1]]);
+        }
+        return transformedCoords;
+    }
+
+    function flipCoordsHorizontally(coords) {
+        return coords.map(coord => [-coord[0], -coord[1]]); // Placeholder logic
+    }
+
+    function flipCoordsVertically(coords) {
+        return coords.map(coord => [-coord[0], -coord[1]]); // Placeholder logic
+    }
+}
