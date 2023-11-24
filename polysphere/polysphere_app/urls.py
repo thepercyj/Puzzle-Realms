6482fe10a83563905ac8226a1c63754eee9952ab
Rez27 <kadam09rohan@gmail.com>
2023-11-24 22:04:15 +0000

    Added Basic Kanoodle 3-D
    Inspiration - https://github.com/benelder/kanoodle-3d
    
    Co-Authored-By: Aman Thapa Magar (PJ) <20582994+thepercyj@users.noreply.github.com>

--- polysphere/polysphere_app/urls.py
+++ polysphere/polysphere_app/urls.py
@@ -6,5 +6,6 @@ from . import views
 urlpatterns = [
     path('solutions/', views.generate_solution_gallery, name='generate_solution_gallery'),
     path('solutions/find_partial_solutions/', views.find_partial_solutions, name='find_partial_solutions'),
+    path('kanoodle_3d/', views.kanoodle_3d, name='kanoodle_3d'),
     path('', views.landing, name='landing'),
 ]
