e5d0ac48ca6e132f61599ecd4a6e1881175c68f1
Aman Thapa Magar (PJ) <aman.yhbs@gmail.com>
2023-12-10 20:54:35 +0000

    Updating CSS and HTML elements for Task 5
    Updated 10/12/2023

--- portal/portal_app/urls.py
+++ portal/portal_app/urls.py
@@ -8,6 +8,11 @@ from . import views
 
 urlpatterns = [
     path('', views.portal, name='portal'),
+    path('nqueens_game/', views.nqueens_game, name='nqueens_game'),
+    path('polysphere_game/', views.polysphere_game, name='polysphere_game'),
+    path('polysphere3D_game/', views.polysphere3D_game, name='polysphere3D_game'),
+    path('team/', views.team, name='team'),
+    path('games/', views.games, name='games'),
     path('login/', LoginView.as_view(template_name='portal_app/login.html', next_page='portal', authentication_form=CustomUserLoginForm),
          name='login'),
     path('logout/', LogoutView.as_view(next_page='portal'), name='logout'),
