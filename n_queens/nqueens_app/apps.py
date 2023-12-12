3914107fa77f2b34d8e6aae06c558e36015c3733
Aman Thapa Magar (PJ) <aman.yhbs@gmail.com>
2023-12-12 03:35:11 +0000

    Updated added levels for Task 3 Polysphere Puzzle
    - Added Levels and Game Board state for Polysphere puzzle
    - Fixed pending bugs posted by QA team for the rotations and flip not working together
    - Removed unwanted files that can confuse other members. Keeping clean directory.
    
    Updated on 12/12/2023

--- n_queens/nqueens_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-# Function which calls the application as "nqueens_app" for this Django Project.
-class NqueensAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'nqueens_app'
--- portal/polysphere3D_app/views.py
+++ portal/polysphere3D_app/views.py
@@ -1,16 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 # Create your views here.
+def landing(request):
+    if request.method == 'POST':
+        data_from_landing = request.POST.get('data_from_landing', '')
+        request.session['data_from_landing'] = data_from_landing
+        return redirect('polysphere3D:levels')
+    else:
+        return render(request, 'polysphere3D_app/landing.html')
+
+
+def levels(request):
+    data_from_landing = request.session.get('data_from_landing', '')
+    return render(request, 'polysphere3D_app/levels.html', {'data_from_landing': data_from_landing})
 
 
 def ui_template(request):
-    if request.method == 'POST':
-        data_from_landing = request.POST.get('data_from_landing', '')
-        return render(request, 'polysphere3D_app/polysphere3d.html', {'data_from_landing': data_from_landing})
-    else:
-        return render(request, 'polysphere3D_app/polysphere3d.html', {'data_from_landing': ''})
-
-
-def landing(request):
-    return render(request, 'polysphere3D_app/landing.html')
+    data_from_landing = request.session.get('data_from_landing', '')
+    return render(request, 'polysphere3D_app/polysphere3d.html', {'data_from_landing': data_from_landing})
