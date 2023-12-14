84b93d2885c5a5d2cef73d768852579ef0d6b59f
Aman Thapa Magar (PJ) <aman.yhbs@gmail.com>
2023-12-14 16:27:49 +0000

    removed the polysphere3D to take from tom latest
    Updated on 14/12/2023

--- portal/polysphere3D_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-
-urlpatterns = [
-    path('ui_template/', views.ui_template, name='ui_template'),
-    path('', views.landing, name='landing'),
-]
