4e8d6261089070d3d6668854148b5eb359de303e
Mya2328 <myathuzarlwin01@gmail.com>
2023-12-15 02:25:16 +0000

    Removing Duplicate applications outside of portal

--- polysphere3D/polysphere3D_app/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.landing, name='landing'),
-    path('ui_template/', views.ui_template, name='ui_template'),
-]
